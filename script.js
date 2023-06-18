be71bb8f40655caf94728c9f97e235e549b304cc
Hannah Allan <hannahallan94@gmail.com>
2023-06-18 17:47:42 +0100

    Changed Quantity Button + JS
    added javascript to increase and decrease quantity

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-let cartEmpty = document.querySelector(".cart-empty");
+//* Your Cart is Empty DIV
 
+let cartEmpty = document.querySelector(".cart-empty");
 
 function showEmpty  (){
 	cartEmpty.classList.toggle("hide");
 }
+
+//* Increase & Decrease Quantity 
+
+
+   const plus = document.querySelector(".plus"),
+   minus = document.querySelector(".minus"),
+   num = document.querySelector(".num");
+
+   let a = 0
+
+   plus.addEventListener("click", ()=> {
+   	a++;
+   	num.innerText=a;
+   	console.log(a);
+   });
+
+    minus.addEventListener("click", ()=> {
+   	if (a > 0) {
+   	a--;
+   	num.innerText=a;
+   	console.log(a);
+   	}
+   });

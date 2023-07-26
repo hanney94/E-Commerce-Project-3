616cd0d814b5932cac5e8e37c1c981115a6d055c
Hannah Allan <hannahallan94@gmail.com>
2023-07-26 17:28:53 +0100

    Input Mobile Image Slideshow

--- script.js
+++ script.js
@@ -148,4 +148,28 @@ function closeNav() {
   document.getElementById("navbar-nav").style.display = "none";
 }
 
+/* Images Slideshow */
+
+
+let slideIndex = 1
+showSlides(slideIndex)
+
+
+function plusSlides(n){
+	showSlides(slideIndex += n);
+}
+
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("product");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  
+  slides[slideIndex-1].style.display = "block"; 
+  
+}
 
